98a270037cf07ab594379a816fd632ddf6ba6a17
sandyrnelson <sandyrnelson@berkeley.edu>
2021-01-12 10:49:38 +0100

    db: add maps insert

---
+++ database/data_dump/maps.sql
@@ -0,0 +1,10 @@
+INSERT INTO maps (name, zoom,  longitude_center, latitude_center, handicap, rounds)
+
+VALUES
+('Africa', 2.5, 15, 2, 2000, 5),
+('USA', 3.2, -96, 40, 2000, 5),
+('South America', 2.47, -58, -25, 1500, 5),
+('Australia', 3.6, 134, -29, 2000, 5),
+('Europe', 2.75, 15, 57, 1000, 5),
+('Asia', 2.2, 86, 45, 1000, 5),
+('Hungary', 6.3, 19.5, 47, 150, 5);
