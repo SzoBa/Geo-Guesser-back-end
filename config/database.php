94d5de44b287676c3d234c9b09e646f4b7318cba
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-14 15:35:06 +0100

    devops: change cors and database to deploy with postgres

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pqsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -61,11 +61,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'geo_junkies'),
+            'username' => env('DB_USERNAME', 'dell'),
+            'password' => env('DB_PASSWORD', '12345'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
