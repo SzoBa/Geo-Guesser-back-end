1cf5b4c5d7e369f741775c78e7680bec1e593c35
sandyrnelson <sandyrnelson@berkeley.edu>
2021-01-27 11:54:03 +0100

    refactor: remove destroy method (unused)

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -71,15 +71,4 @@ class RegistrationController extends Controller
     {
         //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
