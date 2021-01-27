541f40c99f486d2775177b65f70099aa21cb6c91
sandyrnelson <sandyrnelson@berkeley.edu>
2021-01-27 11:55:17 +0100

    refactor: remove index method (unused)

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -9,15 +9,6 @@ use Illuminate\Support\Facades\Validator;
 
 class RegistrationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
