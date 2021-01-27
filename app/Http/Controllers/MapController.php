7c640759d56a51c5ecd4093bcd89bf7e366341c3
sandyrnelson <sandyrnelson@berkeley.edu>
2021-01-27 11:41:48 +0100

    refactor: remove store method (unused)

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -18,16 +18,6 @@ class MapController extends Controller
         return response(Map::all(), 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
