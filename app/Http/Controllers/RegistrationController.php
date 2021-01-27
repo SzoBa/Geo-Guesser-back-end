4764671dfd849b265334b8bb743cf4eaf018888e
sandyrnelson <sandyrnelson@berkeley.edu>
2021-01-27 11:54:18 +0100

    refactor: remove update method (unused)

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -60,15 +60,4 @@ class RegistrationController extends Controller
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
 }
