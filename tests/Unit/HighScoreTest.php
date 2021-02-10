9d7259891762f62874035b6c8e0c16dccf5fcd45
sandyrnelson <sandyrnelson@berkeley.edu>
2021-02-10 14:53:28 +0100

    testing: add test for status in HighScoreController

--- tests/Unit/HighScoreTest.php
+++ tests/Unit/HighScoreTest.php
@@ -3,9 +3,46 @@
 
 namespace Tests\Unit;
 
+use App\Http\Controllers\HighscoreController;
+use App\Http\Controllers\LoginController;
+use Illuminate\Http\Request;
 use Tests\TestCase;
 
 class HighScoreTest extends TestCase
 {
+
+    public function testHighScoreApi_Index_Status()
+    {
+        $response = $this->get('/api/highscores/1');
+        $response->assertStatus(201);
+    }
+
+    public function testHighScoreApi_getById_Status_notLoggedIn()
+    {
+        $response = $this->get('/api/highscore/1');
+        $response->assertStatus(500);
+    }
+
+    public function testHighScore_getById_Status_loggedIn()
+    {
+        $loggedIn = $this->json('post', 'api/login', [
+                'email' => 'sandybeach294@yahoo.com',
+                'password'=>'password'
+            ]);
+
+        $token = $loggedIn["token"];
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
+            ->json('get', 'api/highscore/1', [
+                'email' => 'sandybeach294@yahoo.com',
+            ]);
+
+        $response->assertStatus(200);
+    }
+
+
+    private function registerTestUser(){
+
+    }
+
 
 }
